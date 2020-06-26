f188da6e0eae17b11f75161472429cd9584a1163
Ynda Jas <yndajas@gmail.com>
2020-06-26 15:05:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index)
-  board[index] != " "
+  board[index] != " " || ""
 end
 
 # return board[index] == (" " || "" || nil) do
